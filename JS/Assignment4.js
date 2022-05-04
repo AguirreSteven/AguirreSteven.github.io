b83259ed2aed65db890ae6c21cf1dd1ba467b251
AguirreSteven <stevenaguirre1@icloud.com>
2022-05-03 23:30:35 -0700

    more Assignment 4 stuff

---
+++ JS/Assignment4.js
@@ -0,0 +1,13 @@
+function Bigger() {
+    let txt = document.getElementById('text');
+    txt.style.fontSize = '24pt';
+    alert('Hello, World!');
+    
+
+}
+
+function fancyschmancy() {
+    let txt = document.getElementById('text');
+    txt.style.fontWeight = 'bold';
+    txt.style.color = ''
+}
