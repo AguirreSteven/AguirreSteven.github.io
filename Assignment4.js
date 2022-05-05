50e3af2189d9e6bc9304d0a074a25bff7e3c9197
AguirreSteven <stevenaguirre1@icloud.com>
2022-05-04 22:28:09 -0700

    a

--- Assignment4.js
+++ Assignment4.js
@@ -1,18 +1,18 @@
 function bigger() {
     document.getElementById('textbox').style.fontSize = "24pt";
-    alert('Hello, World!');
+    window.alert('Hello, World!');
 
 }
 
 function fancyschmancy() {
-    let txt = document.getElementById('textbox');
+    var txt = document.getElementById('textbox');
     txt.style.fontWeight = 'bold';
     txt.style.color = '#0000FF';
     txt.style.textDecoration = 'underline';
 }
 
 function boringbetty() {
-    let txt = document.getElementById('textbox');
+    var txt = document.getElementById('textbox');
     txt.style.fontWeight = 'normal';
 }   
 
